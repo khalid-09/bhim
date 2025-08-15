193c32feae03cac77cb516252f027f5e9d26baf3
Khalid Ansari <khaliljpc@gmail.com>
2025-08-15 17:08:23 +0530

    add: google button in root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import GoogleSignInButton from "@/components/google-signin-button";
 
 const Page = () => {
   return (
     <section className="flex min-h-dvh flex-col items-center justify-center gap-4">
       <h1>BHIM</h1>
-      <div className="flex items-center gap-3">
-        <Button asChild>
-          <Link href="/sign-up">Sign Up</Link>
-        </Button>
-        <Button variant="outline" asChild>
-          <Link href="/log-in">Login</Link>
-        </Button>
-      </div>
+      <GoogleSignInButton />
     </section>
   );
 };
