6b95d6a2f20a044a9ca1d3bda881e5c2caeb5402
Khalid Ansari <khaliljpc@gmail.com>
2025-08-15 16:14:05 +0530

    add: homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Page = () => {
   return (
-    <div>
-      <Button className="text-sm md:text-base">Hello world</Button>
-    </div>
+    <section className="flex min-h-dvh flex-col items-center justify-center gap-4">
+      <h1>BHIM</h1>
+      <div className="flex items-center gap-3">
+        <Button asChild>
+          <Link href="/sign-up">Sign Up</Link>
+        </Button>
+        <Button variant="outline" asChild>
+          <Link href="/log-in">Login</Link>
+        </Button>
+      </div>
+    </section>
   );
 };
 
