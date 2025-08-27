88bb857126cb260656823051af592d86882073e5
Khalid Ansari <khaliljpc@gmail.com>
2025-08-27 17:18:42 +0530

    add: work log validation schema

---
+++ src/lib/validation/work-log.ts
@@ -0,0 +1,34 @@
+import { workLog } from "@/db/schema";
+import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
+
+const baseWorkLogSchema = createInsertSchema(workLog, {
+  machineNo: (schema) => schema.min(1, "Machine No. is required."),
+  taar: (schema) => schema.min(1, "Taar is required."),
+  karigarName: (schema) => schema.min(1, "Karigar name is required."),
+  qualityId: z.uuid({ message: "Company is required." }),
+}).omit({
+  id: true,
+  userId: true,
+  createdAt: true,
+});
+
+export const createWorkLogSingleCompanySchema = baseWorkLogSchema.omit({
+  companyId: true,
+});
+
+export const createWorkLogDashboardSchema = baseWorkLogSchema.extend({
+  companyId: z.uuid("Company is required."),
+});
+
+export const createWorkLogServerSchema = baseWorkLogSchema.extend({
+  companyId: z.uuid("Company is required."),
+});
+
+export type CreateWorkLogSingleCompany = z.infer<
+  typeof createWorkLogSingleCompanySchema
+>;
+export type CreateWorkLogDashboard = z.infer<
+  typeof createWorkLogDashboardSchema
+>;
+export type CreateWorkLogServer = z.infer<typeof createWorkLogServerSchema>;
