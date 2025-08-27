50b77056a9daad70dc3b86a0f42df4d672c30c2d
Khalid Ansari <khaliljpc@gmail.com>
2025-08-27 13:59:25 +0530

    add: utility fns

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,34 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { format } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const convertDate = (
+  date: Date | string | undefined,
+  formatString: string = "dd/MM/yyyy",
+): string => {
+  if (!date) return "";
+  const dateObj = typeof date === "string" ? new Date(date) : date;
+
+  if (isNaN(dateObj.getTime())) return "";
+
+  return format(dateObj, formatString);
+};
+
+export const formatPrice = (price: number) => {
+  const formatOptions: Intl.NumberFormatOptions = {
+    style: "currency",
+    currency: "INR",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 3,
+  };
+
+  const formattedPrice = new Intl.NumberFormat("en-IN", formatOptions).format(
+    price,
+  );
+
+  return formattedPrice;
+};
